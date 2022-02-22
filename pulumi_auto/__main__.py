b473b1125eac36a51079db553f452535776dee14
Hanna Shyshkova <hshyshkova-ext@ceteriscloud.onmicrosoft.com>
2022-02-22 20:20:10 +0000

    Updated __main__.py loops for pipelines added and variables replaced

--- pulumi_auto/__main__.py
+++ pulumi_auto/__main__.py
@@ -15,8 +15,12 @@ from htw_pulumi_dataset import createDatasetADLSAndReturn
 from htw_pulumi_dataset import createDatasetASQLAndReturn
 from htw_pulumi_db import getMetaTable
 import htw_pulumi_pipelines as pipe
+import pandas as pd
 #Gedanken über Benamung
 #import pulumi_azure as Azure
+
+psw="OZh2fwL3TUqSzFO0fwfc"
+userid="Team4Admin"
 
 # Create an Azure Resource Group This Pulumi program creates an Azure resource group and storage account and then exports the storage account’s primary key.
 resource_group = resources.ResourceGroup('resource_group',
@@ -69,8 +73,8 @@ factory = azure_native.datafactory.Factory("factory",
 #SqlServer
 
 server = azure_native.sql.Server("server",
-    administrator_login="Team4Admin",
-    administrator_login_password="OZh2fwL3TUqSzFO0fwfc", #später lösen, sodass sie nicht mehr im sourcecode sind
+    administrator_login=userid,
+    administrator_login_password=psw, #später lösen, sodass sie nicht mehr im sourcecode sind
     resource_group_name=resource_group.name,
     server_name="htw-cet-sqlserver",
     public_network_access = "Enabled",
@@ -127,66 +131,76 @@ database = azure_native.sql.Database("dbsource1",
     #ConfigDatenbank Basic Tier 2-3 tabellen aufnehmen (Linked Service)
 
 #Hanna: Pipeline erst ohne Parameter
-#general info
-factory_name_auto = "htwcetdatafactory"
-resource_group_name_auto="pulumiauto"
-psw="OZh2fwL3TUqSzFO0fwfc"
-userid="Team4Admin"
 
-#Source auto
-source_server_name_sql_auto = server.name
-source_server_port_sql_auto = "1433"
-source_server_alias_sql_auto = database.name
+# load meta_table
+meta_table = getMetaTable()
+# create a dataframe object from dict
+df_meta_table = pd.DataFrame(meta_table)
 
 # Blob auto
-blob_account_name_auto = "plsource1"
+blob_account_name_auto = "plsource1"#account_source.name
 blob_account_key_auto = "amu31xt5j91azbjZsACM5VaDMIIEnIj8Y3aYOKmX1aCbdLRCJxI/lLNpbno/X1nKHlPxyYfi3v3aSakEKB6Gpw=="
 
 # Datalake auto
-data_lake_account_name_auto = "pldestination"
+data_lake_account_name_auto = "pldestination"#account_dest.name
 data_lake_account_key_auto = "SOKULBYhq4QRX6XeVDKZTnoYffi9iPZeEmKqLArrt+5gPl+MP+46AOp3V6r/wBxq2jEK+xiZd87v1/XJILwk1A=="
 
 #linked service names
-linked_service_name_sql="LS_ASQL_SalesLT"
+linked_service_name_sql="LS_ASQL_DB"
 linked_service_name_blob ="LS_ABLB_CSV"
 linked_service_name_datalake ="LS_ADLS_Target"
 
 # create linked services
-linked_service_sql_db2 = createLSSourceASQLandReturn(factory_name_auto,linked_service_name_sql,source_server_name_sql_auto,source_server_port_sql_auto,source_server_alias_sql_auto,userid,psw,resource_group_name_auto)
-linked_service_blob =createLSABLBandReturn(factory_name_auto,linked_service_name_blob,blob_account_name_auto, blob_account_key_auto,resource_group_name_auto)
-linked_service_datalake = createLSTargetADLSandReturn(factory_name_auto,linked_service_name_datalake,data_lake_account_name_auto, data_lake_account_key_auto,resource_group_name_auto)
+linked_service_sql_db2 = createLSSourceASQLandReturn(factory.name,linked_service_name_sql,server.name,"1433",database.name,userid,psw,resource_group.name)
+linked_service_blob =createLSABLBandReturn(factory.name,linked_service_name_blob,account_source.name, blob_account_key_auto,resource_group.name)
+linked_service_datalake = createLSTargetADLSandReturn(factory.name,linked_service_name_datalake,account_dest.name, data_lake_account_key_auto,resource_group.name)
 
 # create datasets
-dataset_asql = createDatasetASQLAndReturn("SalesLT",factory_name_auto,linked_service_sql_db2,resource_group_name_auto)
-dataset_blob = createDatasetABLBAndReturn("Email",factory_name_auto,linked_service_blob,resource_group_name_auto)
-dataset_dl_archiv = createDatasetADLSAndReturn("Archiv",factory_name_auto,linked_service_datalake,resource_group_name_auto)
-dataset_dl_import = createDatasetADLSAndReturn("Import",factory_name_auto,linked_service_datalake,resource_group_name_auto)
-dataset_dl_temp = createDatasetADLSAndReturn("Temp",factory_name_auto,linked_service_datalake,resource_group_name_auto)
+"""CREATE DATASET FOR WHOLE DATABASE"""
+dataset_asql = createDatasetASQLAndReturn("DB",factory.name,linked_service_sql_db2,resource_group.name)
+
+"""CREATE DATASETs FOR EACH CSV FILE (NAME OF CSV FILE SHOULD BE EQUAL TO NAME IN SQL TABLE)"""
+# save table names for csv tables
+table_names_csv=df_meta_table.loc[df_meta_table['table_type']=="CSV"]['table_name'].values.tolist()
+
+datasets_blob_csv_auto=[]
+
+for table_name_csv in table_names_csv:
+    # create dataset csv
+    dataset_blob_csv=createDatasetABLBAndReturn(table_name_csv,factory.name,linked_service_blob,resource_group.name)
+    # add dataflow into data_flows_auto list
+    datasets_blob_csv_auto.append({'table_name': table_name_csv,
+                            'dataset_obj': dataset_blob_csv
+                            })
+"""CREATE DATASETs ARVHIV, IMPORT AND TEMP FOLDERS IN BLOB STORAGE"""
+dataset_dl_archiv = createDatasetADLSAndReturn("Archiv",factory.name,linked_service_datalake,resource_group.name)
+dataset_dl_import = createDatasetADLSAndReturn("Import",factory.name,linked_service_datalake,resource_group.name)
+dataset_dl_temp = createDatasetADLSAndReturn("Temp",factory.name,linked_service_datalake,resource_group.name)
 
 # create dataflows
-meta_table=getMetaTable()
+
 data_flows_auto=[]
+
 for x in meta_table:
+    # create dataflows
+    data_flow = createDataFlowAndReturn(x['table_name'],x['key_column'],factory.name,resource_group.name,linked_service_datalake)
+    # add dataflow into data_flows_auto list
     data_flows_auto.append({'table_name':x['table_name'],
-                            'data_flow_obj': createDataFlowAndReturn(x['table_name'],x['key_column'],factory_name_auto,resource_group_name_auto,linked_service_datalake)
+                            'data_flow_obj': data_flow
                             })
-print(data_flows_auto)
-test = data_flows_auto[0]['data_flow_obj']
 
-print(type(test.name))
+"""CREATE PIPELINES"""
+"""
+df_schema_list = df_meta_table['table_schema'].loc[df_meta_table['table_schema']!='Manual'].unique()
+print(df_schema_list)
 
-#data_flow_csv_email = createDataFlowAndReturn("Email","Identifier",factory_name_auto,resource_group_name_auto,linked_service_datalake)
-#data_flow_asql_product = createDataFlowAndReturn("Product","ProductID",factory_name_auto,resource_group_name_auto,linked_service_datalake)
-#data_flow_asql_address = createDataFlowAndReturn("Address","AddressID",factory_name_auto,resource_group_name_auto,linked_service_datalake)
+for schema in df_schema_list:
+    print(schema)
+    table_names_sql=df_meta_table.loc[df_meta_table['table_schema']==schema]['table_name'].values.tolist()
+    print(table_names_sql)
+    pipe.create_custom_sql_source_pipelines(table_names_sql,dataset_asql.name,"parquet","azuresql",linked_service_sql_db2.name,schema,"[dbo].[UpdateErrorTable]","[dbo].[usp_write_watermark]",dataset_dl_archiv.name,dataset_dl_temp.name,"parquet","parquet") 
+"""
+"""for dataset in datasets_blob_csv_auto:
+    pipe.create_custom_sql_source_pipelines([dataset['table_name']],dataset['dataset_obj'].name,"parquet","azuresql",linked_service_blob.name,"","[dbo].[UpdateErrorTable]","[dbo].[usp_write_watermark]",dataset_dl_archiv.name,dataset_dl_temp.name,"parquet","parquet") 
+"""
 
-
-table_names=["Product", "Address", "Customer", "CustomerAddress", "Email", "ProductCategory", "ProductDescription", "ProductModel", "ProductModelProductDescription", "SalesOrderDetail", "SalesOrderHeader"]
-sql_dataset="DS_ASQL_DB"
-sql_sink_type="parquet" # unterscheidung csv sql
-sql_source_type="azuresql"
-sql_linked_service="LS_ASQL_SalesLT"
-schema="SalesLT"
-archiv_dataset="DS_ADLS_Archiv"
-temp_dataset="DS_ADLS_Temp"
-
-pipe.create_custom_sql_source_pipelines() 
