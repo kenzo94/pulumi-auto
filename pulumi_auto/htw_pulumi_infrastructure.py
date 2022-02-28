2fb159c24800aad5d694e23b7ef92942fc561a29
yyasemin <ysmn_yelken@hotmail.com>
2022-02-28 11:38:30 +0100

    infracreate

--- pulumi_auto/htw_pulumi_infrastructure.py
+++ pulumi_auto/htw_pulumi_infrastructure.py
@@ -5,21 +5,33 @@ import pulumi_azure_native as azure_native
 from pulumi_azure_native import storage
 from pulumi_azure_native import resources
 
-psw="OZh2fwL3TUqSzFO0fwfc"
-userid="Team4Admin"
+def getpassword():
+    return "OZh2fwL3TUqSzFO0fwfc"
+
+def getuserid():
+    return "Team4Admin"
+
+
+
+#Infratsurcture als Methode definieren create
 
 # Create an Azure Resource Group This Pulumi program creates an Azure resource group and storage account and then exports the storage account’s primary key.
-resource_group = resources.ResourceGroup('resource_group',
- resource_group_name='pulumiauto')
+def createResourceGroup(resource_group_name):
+    resource_group = resources.ResourceGroup('resource_group_'+resource_group_name,
+        resource_group_name=resource_group_name)
 
 # Create an Azure resource (Storage Account)
-account_source = storage.StorageAccount('cethtwstorage',   
-    account_name='plsource1',
-    resource_group_name=resource_group.name,
-    sku=storage.SkuArgs(
-        name=storage.SkuName.STANDARD_LRS,
-    ),
-    kind=storage.Kind.STORAGE_V2)
+
+def createAccoutSource(account_source_name, resource_group_name):
+    account_source = storage.StorageAccount('account_source_'+account_source_name,   
+        account_name=account_source_name,
+        resource_group_name=resource_group_name,
+        sku=storage.SkuArgs(
+            name=storage.SkuName.STANDARD_LRS,
+        ),
+        kind=storage.Kind.STORAGE_V2)
+
+#create Methoden ab hier
 
     #BlobContainer
 blob_container = azure_native.storage.BlobContainer("blobContainer",
@@ -59,8 +71,8 @@ factory = azure_native.datafactory.Factory("factory",
 #SqlServer
 
 server = azure_native.sql.Server("server",
-    administrator_login=userid,
-    administrator_login_password=psw, #später lösen, sodass sie nicht mehr im sourcecode sind
+    administrator_login=getuserid(),
+    administrator_login_password=getpassword(), #später lösen, sodass sie nicht mehr im sourcecode sind
     resource_group_name=resource_group.name,
     server_name="htw-cet-sqlserver",
     public_network_access = "Enabled",
@@ -112,3 +124,40 @@ database_source = azure_native.sql.Database("dbsource1",
     requested_backup_storage_redundancy="Local",
     sample_name="AdventureWorksLT"
     )
+
+
+#factoryname
+
+#servername
+#account destname
+#account sourcename
+#keys destname
+#keys sourcename
+#database sourcename (Dbsource1)
+#groupname
+
+
+resource_group_name = azure_native.resources.get_resource_group(resource_group_name=resource_group_name)
+print(resource_group.name) 
+factory= azure_native.datafactory.get_factory(factory_name="htwcetdatafactory",resource_group_name=resource_group.name)
+print(factory.name)
+
+def getFactoryName(resource_group_name, factory_name):
+    factory= azure_native.datafactory.get_factory(factory_name=factory_name,resource_group_name=resource_group_name)
+    return factory.name
+
+server= azure_native.sql.get_server(resource_group_name=resource_group.name,server_name="htw-cet-sqlserver")
+print(server.name)
+database_dwh=azure_native.sql.get_database(database_name="DWH", resource_group_name=resource_group.name,server_name=server.name)#,"","htw-cet-sqlserver")
+print(database_dwh.name)
+
+database_source=azure_native.sql.get_database(database_name="DBSource1", resource_group_name=resource_group.name,server_name=server.name)
+print(database_source.name)
+account_source = storage.get_storage_account(account_name='plsource1',resource_group_name=resource_group.name)
+blob_account_keys = storage.list_storage_account_keys(account_name='plsource1',resource_group_name=resource_group.name)
+blob_account_key= blob_account_keys.keys[0]['value']
+print(blob_account_keys.keys[0]['value'])
+account_dest  = storage.get_storage_account(account_name='plsource1',resource_group_name=resource_group.name)
+data_lake_keys = storage.list_storage_account_keys(account_name='pldestination',resource_group_name=resource_group.name)
+data_lake_account_key= data_lake_keys.keys[0]['value']
+print(data_lake_account_key)
